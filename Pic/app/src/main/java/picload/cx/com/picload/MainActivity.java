73def11501af81c746af768faf0b46024c8e9fd8
xingege662 <924717778@qq.com>
2017-02-07 22:23:12 +0800

    创建项目

---
+++ Pic/app/src/main/java/picload/cx/com/picload/MainActivity.java
@@ -0,0 +1,13 @@
+package picload.cx.com.picload;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
